1244ac8801cf3e7407cdb200887ffdd3485455c2
lucianovalente85 <luciano.valente@viannasempre.com.br>
2020-02-14 22:27:54 -0300

    Criando os primeiros arquivos do model

---
+++ Blog/Models/Blog/Categoria/CategoriaEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Categoria
+{
+    public class CategoriaEntity
+    {
+        public string nome { get; set; }
+    }
+}
--- Blog/Models/Blog/Postagem/Postagem.cs
+++ Blog/Models/Blog/Postagem/Postagem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,8 @@ namespace Blog.Models.Blog.Postagem
 {
     public class Postagem
     {
+        public string titulo { get; set; }
+        public AutorEntity autor { get; set; }
+        public CategoriaEntity categoria { get; set; }
     }
 }
